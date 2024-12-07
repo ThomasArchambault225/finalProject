0e5590cfc56901177db52bc94024a58a60ea23e8
ThomasArchambault225 <tomarchambault1@gmail.com>
2024-12-07 12:48:40 -0500

    Initial

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -88,3 +88,77 @@ public class Book {
         }
     }
 
+    /**
+     * This method converts the title and author name to title case.
+     * Each word's first letter will be capitalized.
+     */
+    public void toTitleCase() {
+        this.title = convertToTitleCase(this.title);
+        this.author = convertToTitleCase(this.author);
+    }
+
+    /**
+     * Converts a string into title case (capitalizes the first letter of each word).
+     *
+     * @param text The string to be converted to title case.
+     * @return The title-case string.
+     */
+    private String convertToTitleCase(String text) {
+        String[] words = text.split(" ");
+        StringBuilder titleCase = new StringBuilder();
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                titleCase.append(word.substring(0, 1).toUpperCase())
+                        .append(word.substring(1).toLowerCase())
+                        .append(" ");
+            }
+        }
+
+        return titleCase.toString().trim();
+    }
+
+    /**
+     * This method returns a string representation of the book.
+     *
+     * @return A string containing the title, author, price, publisher, and ISBN.
+     */
+    @Override
+    public String toString() {
+        return "Title: " + title + "\n" +
+                "Author: " + author + "\n" +
+                "Price: " + price + "\n" +
+                "Publisher: " + publisher + "\n" +
+                "ISBN: " + isbn;
+    }
+
+    /**
+     * This method checks if two Book objects are equal based on their attributes.
+     *
+     * @param obj The object to compare with.
+     * @return true if the two books are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Book book = (Book) obj;
+        return title.equals(book.title) &&
+                author.equals(book.author) &&
+                Double.compare(book.price, price) == 0 &&
+                publisher.equals(book.publisher) &&
+                isbn.equals(book.isbn);
+    }
+
+    /**
+     * This method creates and returns a clone of the current book object.
+     *
+     * @return A new Book object with the same attributes as this book.
+     */
+
