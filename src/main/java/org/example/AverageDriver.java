bcb8f647009131d2b590ba28698faf03179d59be
ThomasArchambault225 <tomarchambault1@gmail.com>
2024-12-06 21:31:19 -0500

    Initial

---
+++ src/main/java/org/example/AverageDriver.java
@@ -0,0 +1,10 @@
+package org.example;
+public class AverageDriver {
+    public static void main(String[] args) {
+        // Create an instance of the Average class
+        Average average = new Average();
+
+        // Print the Average object information
+        System.out.println(average);
+    }
+}
